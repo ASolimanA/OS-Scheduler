26d30a45ca3f7049ca1d5e1ee132410e9c92ac98
HishamHatem <heshamhatem08@gmail.com>
2025-04-13 00:19:20 +0200

    Adding some feature to GUI

--- frontend/mainwindow.cpp
+++ frontend/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "toggle.h"
+#include "qcombobox.h"
+#include "QString"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,9 +10,34 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     new ToggleSwitch(ui->toggle_button);
+    //comboBox
+    QStringList options = {"FCFS","SJF","Priority","Round Robin"};
+    ui->comboBox->addItems(options);
+    ui->comboBox->setFixedWidth(300);
+    ui->comboBox->setEditable(false);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_comboBox_currentTextChanged(const QString &arg1)
+{
+    if (arg1 == "FCFS" || arg1 == "Round Robin") {
+        ui->non_preemptive->setEnabled(false);
+        ui->preemptive->setEnabled(false);
+        ui->non_preemptive->setChecked(true);
+    } else {
+        ui->non_preemptive->setEnabled(true);
+        ui->preemptive->setEnabled(true);
+    }
+    if (arg1 == "FCFS" || arg1 == "SJF"){
+        ui->lineEdit_3->setEnabled(false);
+    }
+    else{
+        ui->lineEdit_3->setEnabled(true);
+    }
+}
+
