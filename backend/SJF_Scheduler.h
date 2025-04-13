1e5e92848760d2747a390231335879d60397b076
janaa-wael <janawael119@gmail.com>
2025-04-14 00:36:26 +0200

    Adding SJF_Scheduler.h file

---
+++ backend/SJF_Scheduler.h
@@ -0,0 +1,30 @@
+#ifndef SJF_SCHEDULER_H
+#define SJF_SCHEDULER_H
+
+#include <iostream>
+#include <memory>
+#include "scheduler.h"
+#include "process.h"
+
+class SJF_Sscheduler : public Scheduler {
+private:
+    struct CompareRemainingTime {
+        bool operator()(const std::shared_ptr<Process>& a, const std::shared_ptr<Process>& b) const {
+            if (a->getRemainingTime() == b->getRemainingTime()) {
+                return a->getArrivalTime() > b->getArrivalTime();
+            }
+            return a->getRemainingTime() > b->getRemainingTime();
+        }
+    };
+
+public:
+    SJF_Sscheduler();
+
+    virtual void run(int runUntilTime = -1) override;
+
+    virtual std::shared_ptr<Process> selectNextProcess() override;
+};
+
+
+
+#endif /* SJF_SCHEDULER_H_ */
